a8541d7e479b2bd155741b98d6aa0cf843b81859
alexandrudsc <alexandru.dascalu94@gmail.com>
2018-09-30 14:04:06 +0300

    [CHANGE] orar_16 updated about section
    unit test

--- app/src/main/java/com/developer/alexandru/orarusv/AboutFragment.java
+++ app/src/main/java/com/developer/alexandru/orarusv/AboutFragment.java
@@ -2,9 +2,12 @@ package com.developer.alexandru.orarusv;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by Alexandru on 9/13/2014.
@@ -15,6 +18,16 @@ public class AboutFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+        TextView textDescription = (TextView) view.findViewById(R.id.about_description);
+        textDescription.setText(Html.fromHtml(
+                inflater.getContext().getResources().getString(R.string.about_link)));
+        textDescription.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView textGitRepo = (TextView) view.findViewById(R.id.about_git);
+        textGitRepo.setText(Html.fromHtml(
+                inflater.getContext().getResources().getString(R.string.about_git_rep)));
+        textGitRepo.setMovementMethod(LinkMovementMethod.getInstance());
+        return view;
     }
 }
--- app/src/test/com/test/AboutFragmentTest.java
+++ app/src/test/com/test/AboutFragmentTest.java
@@ -1,9 +1,12 @@
 package com.test;
 
 import android.support.v4.app.Fragment;
+import android.text.Html;
+import android.widget.TextView;
 
 import com.developer.alexandru.orarusv.AboutFragment;
 import com.developer.alexandru.orarusv.BuildConfig;
+import com.developer.alexandru.orarusv.R;
 
 import org.hamcrest.CoreMatchers;
 import org.robolectric.RobolectricTestRunner;
@@ -14,6 +17,7 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
+import org.robolectric.shadows.support.v4.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsNull.notNullValue;
@@ -23,17 +27,33 @@ import static org.robolectric.shadows.support.v4.SupportFragmentTestUtil.startFr
 @Config(constants = BuildConfig.class)
 public class AboutFragmentTest {
 
-    private AboutFragment aboutFragment;
+    private AboutFragment fragment;
 
     @Before
     public void setUp() {
-        aboutFragment = new AboutFragment();
+        fragment = new AboutFragment();
+        SupportFragmentTestUtil.startVisibleFragment(fragment);
     }
 
     @Test
     public void aboutFragmentCreated() {
-        startFragment(aboutFragment);
+        assertThat(fragment.getView(), is(notNullValue()));
+    }
 
-        assertThat(aboutFragment, is(notNullValue()));
+    @Test
+    public void aboutFragmentTextDisplayed() {
+        assertThat(fragment, is(notNullValue()));
+        final TextView tv_description = (TextView)fragment.getView().
+                findViewById(R.id.about_description);
+        assertThat(tv_description, is(notNullValue()));
+        assertThat(tv_description.getText().toString(),
+                is(CoreMatchers.equalTo("Adaptat pentru orar.usv.ro")));
+        final TextView tv_git_repo = (TextView) fragment.getView().
+                findViewById(R.id.about_git);
+        assertThat(tv_git_repo, is(notNullValue()));
+        assertThat(tv_git_repo.getText().toString(),
+                is(CoreMatchers.equalTo(
+                        "Contribuțiile și ideilor tuturor sunt binevenite pe Github sau pe Google Play"
+                )));
     }
 }
